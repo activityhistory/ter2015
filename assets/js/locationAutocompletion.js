e7a6d1c57c0ca1283230a2f2a6d10c0d78e4d901
Audrea M <malialin.audrea@gmail.com>
2015-02-12 21:52:04 +0100

    nouveau fichier: assets/js/changeStateLocation.js 	nouveau fichier: assets/js/locationAutocompletion.js

---
+++ assets/js/locationAutocompletion.js
@@ -0,0 +1,57 @@
+
+
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+
+var placeSearch, autocomplete;
+
+function initialize() {
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {HTMLInputElement} */(document.getElementById('autocomplete')),
+      { types: ['geocode'] });
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    fillInAddress();
+
+  });
+  
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(44.5403, -78.5463),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+
+}
+
+function fillInAddress() {
+  
+  var place = autocomplete.getPlace();
+  
+  document.getElementById('longitudeField').value = place.geometry.location.lng();
+  document.getElementById('latitudeField').value = place.geometry.location.lat();
+  
+  var mapCanvas = document.getElementById('map-canvas');
+  var mapOptions = {
+    center: new google.maps.LatLng(place.geometry.location.lat(), place.geometry.location.lng()),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  
+}
+
+function geolocate() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var geolocation = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+      var circle = new google.maps.Circle({
+        center: geolocation,
+        radius: position.coords.accuracy
+      });
+      autocomplete.setBounds(circle.getBounds());
+    });
+  }
+}
+
